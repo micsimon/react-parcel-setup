cbfc3689d5d76bb677ee28c22f28121d2a4d2abb
Michael Simon <5222293+micsimon@users.noreply.github.com>
2019-11-04 13:00:14 +0100

    addéd init hello world component

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+const asd: string = "";
+
+asd = 1;
+
+function App() {
+    return <div>Hello World1</div>
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
+
+
