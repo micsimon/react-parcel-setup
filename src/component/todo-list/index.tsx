d8d9b061f3c7399e52dcaab1ab7ed047565fc1ee
Michael Simon <5222293+micsimon@users.noreply.github.com>
2019-11-07 08:25:35 +0100

    added todolist component

---
+++ src/component/todo-list/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <ul>
+            <li>1. stand up</li>
+            <li>2. coffee</li>
+            <li>3. code</li>
+        </ul>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import './index.css'
+import ToDoList from './component/todo-list';
 
 function App() {
-    return <div className="header">Hello World1</div>
+    return (
+        <div>
+            <div className="header">Hello World1</div>
+            <ToDoList/>
+        </div>
+    )
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));
