454ac159ee5291621fd1f4cafaf7f79269a2407b
Michael Simon <5222293+micsimon@users.noreply.github.com>
2019-11-08 10:24:41 +0100

    added hook to change state

--- src/component/todo-list/index.tsx
+++ src/component/todo-list/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default () => {
+
+    const [name, setName] = useState('name');
+
+    function onChangeName(value: string) {
+        setName(value);
+    }
+
+    function logCurrentName(event: any) {
+        console.log("###event ", event);
+        console.log("###name ", name);
+    }
+
     return (
-        <ul>
-            <li>1. stand up</li>
-            <li>2. coffee</li>
-            <li>3. code</li>
-        </ul>
+        <div>
+            <ul>
+                <li>1. stand up</li>
+                <li>2. coffee</li>
+                <li>3. code</li>
+                <li>{name}</li>
+            </ul>
+            <input type="text" value={name} onChange={event => onChangeName(event.target.value)}/>
+            <button onClick={event => logCurrentName(event)}>log name</button>
+        </div>
     )
 }
